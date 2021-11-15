05e01e71b5036e0e1350e71568e045a91d56f3f0
Oleksii Verbytskyi <alverba88@gmail.com>
2021-11-15 16:14:30 +0200

    mod header+hero

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,30 +1,29 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
-  
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
-  
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
-    };
-  
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-  
-    // Закрываем мобильное меню на более широких экранах
-    // в случае изменения ориентации устройства.
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    mobMenu: document.querySelector('[data-mobile-menu]'),
+    buttonBuy: document.querySelector('[data-button-buy]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  window.matchMedia('(min-width: 1279px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    if(refs.mobMenu.classList.contains('is-open')){
+      refs.buttonBuy.classList.remove('is-hidden');
+      refs.openModalBtn.classList.remove('is-hidden');
+      refs.mobMenu.classList.toggle('is-open');
       bodyScrollLock.enableBodyScroll(document.body);
-    });
-  })();
-  
+    }  
+    
+  });
+
+  function toggleModal() {
+    refs.openModalBtn.classList.toggle('is-hidden');    
+    refs.buttonBuy.classList.toggle('is-hidden');
+    refs.mobMenu.classList.toggle('is-open');
+    
+  }
+})();
